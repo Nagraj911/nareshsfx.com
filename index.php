81341d7d8f25f0f0fe99c4a350f29cfd1239b604
Nagraj911 <45996949+Nagraj911@users.noreply.github.com>
2018-12-23 14:09:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,134 @@
+<?php include 'header.php';?>
+<!--===================== End of Header ========================-->
+	<!--===================== Base Slider ========================-->
+	<div class="base-slider owl-carousel owl-theme bg-gray">
+		<div class="item">
+			<img src="assets/images/bg-test.png" alt="slider">
+			<div class="inside">
+				<h2>Best Web Hosting For Your Website</h2>
+				<p>get best speed for your website. dont loose more clients.</p>
+				<a href="service-page.html" class="custom-btn">Get Started Now</a>
+			</div><!--inside-->
+		</div>
+		<div class="item">
+			<img src="assets/images/bg-test.png" alt="slider">
+			<div class="inside">
+				<h2>Best Web Hosting For Your Website</h2>
+				<p>get best speed for your website. dont loose more clients.</p>
+				<a href="service-page.html" class="custom-btn">Get Started Now</a>
+			</div><!--inside-->
+		</div>
+	</div>
+	<!--===================== End of Base Slider ========================-->
+	<section class="bg-gray">
+		<div class="container">
+			<!--===================== Partner ========================-->
+			<div class="partner animatedParent">
+				<h1>Welcome to Tvarit Solutions</h1>
+				
+			</div>
+			<!--===================== End of Partner ========================-->
+			<!--===================== Why Choose ========================-->
+			<div class="why-choose animatedParent">
+				<h2 class="title-head">Why you should choose us</h2>
+				<div class="row">
+					<div class="col-md-4 col-xs-12 animated bounceInUp delay-250">
+						<div class="inside">
+							<img src="assets/images/website.svg" alt="website">
+							<a href="webapplicationdevelopment.html" class="text-black font-weight-500">TIMING</a>
+							<p>Our vision is to build scalable and sustainable Automated Predictive Analytics (APA) solutions which are invisible and assistive, allowing manufacturers to do what they love - efficient productions. </p>
+							<a href="webapplicationdevelopment.html" class="read-more">Learn More <img src="assets/images/right.png" alt="right"></a>
+						</div><!--inside-->
+					</div>
+					<div class="col-md-4 col-xs-12 animated bounceInUp delay-500">
+						<div class="inside">
+							<img src="assets/images/powerfull.svg" alt="powerfull">
+							<a href="mobileapplicationdevelopment.html" class="text-black font-weight-500">TECHNOLOGY</a>
+							<p>Through simple observation, we’ve discovered that companies deploying Industrial Internet solutions posses a distinct technical advantage over those who don’t. We want to make factories technologically advanced allowing machines and humans to work more effectively enabling businesses to make smart and informed decisions. 
+ </p>
+							<a href="mobileapplicationdevelopment.html" class="read-more">Learn More <img src="assets/images/right.png" alt="right"></a>
+						</div><!--inside-->
+					</div>
+					<div class="col-md-4 col-xs-12 animated bounceInUp delay-750">
+						<div class="inside">
+							<img src="assets/images/website.svg" alt="website">
+							<a href="cloudapplicationdevelopment.html" class="text-black font-weight-500">TEAM</a>
+							<p>We’re based out of Niedernhausen Germany having the perfect team composition - a German founder bringing vast know-how of machinery coupled with high-quality software expertise of the Indian founders.  </p>
+							<a href="cloudapplicationdevelopment.html" class="read-more">Learn More <img src="assets/images/right.png" alt="right"></a>
+						</div><!--inside-->
+					</div>
+					
+				</div>
+			</div>
+			<!--===================== End of Why Choose ========================-->
+			<!--===================== Hosting Software ========================-->
+			<div class="hosting-software">
+				<h2 class="title-head">Serving in World</h2>
+				<ul id="counter">
+					<li><b class="count" data-count="244">0</b><span>Projects Completed</span></li>
+					<li>1000+<span>Cup a Coffee</span></li>
+					<li><b class="count" data-count="500">0</b><span>Customrs</span></li>
+					<li><b class="count" data-count="500">0</b><span>Happy Clients</span></li>
+				</ul>
+			</div>
+			<!--===================== End of Hosting Software ========================-->
+		</div>
+	</section>
+	<!--===================== Pricing Table ========================-->
+	<div class="pricing-table animatedParent">
+		<div class="container">
+			
+			<div class="info-pricing animated bounceInRight delay-250">
+				<h4>We How to make your life easier and upgraded!</h4>
+				<ul>
+					<li>
+						<h6>Why APA ? –</h6>
+						<p>You must be wondering what is APA and why should you be interested in it? Well, we have got you covered. The First Mover, the Smart worker, The clever predictor, all these titles must be sounding very lucrative. APA brings them all to you. The senior management can run the analytics on fingertips and exercise direct control over the operations through our services. Also reduces energy consumption, reducing the bill up to 12.5%. So, why fall back when we have your back?
+</p>
+					</li>
+				</ul>
+				<ul class="right">
+					<li>
+						<h6>Check out our Products!-</h6>
+						<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</p>
+					</li>
+					<li class="button right">
+						<a href="contact-us.php" class="custom-btn">Get Started Now</a>
+					</li>
+				</ul>
+			</div><!--info-pricing-->
+		</div>
+	</div>
+	<!--===================== End of Pricing Table ========================-->
+	<!--===================== User Slider ========================-->
+<div class="partner animatedParent">
+				<h3>Our Clients</h3>
+				<div class="partner-slider owl-carousel owl-theme">
+					<div class="item animated bounceInLeft delay-250"><a href="#"><img src="assets/images/clients/1.jpg" alt="partner"></a>
+					<div class="info" style="text-align:center !important;">
+                                    
+                                </div>
+					</div>
+					<div class="item animated bounceInLeft delay-500"><a href="#"><img src="assets/images/clients/2.png" alt="partner"></a>
+					<div class="info" style="text-align:center !important;">
+                                  
+                                </div>
+					</div>
+					<div class="item animated bounceInLeft delay-750"><a href="#"><img src="assets/images/clients/3.jpg" alt="partner"></a>
+					<div class="info" style="text-align:center !important;">
+                                   
+                                </div>
+					</div>
+					<div class="item animated bounceInLeft delay-1000"><a href="#"><img src="assets/images/clients/4.jpg" alt="partner"></a>
+					<div class="info" style="text-align:center !important;">
+                                  
+                                </div>
+					</div>
+					<div class="item animated bounceInLeft delay-1000"><a href="#"><img src="assets/images/clients/5.jpg" alt="partner"></a>
+					<div class="info" style="text-align:center !important;">
+                                  
+                                </div>
+					</div>
+				</div>
+			</div>
+<?php include 'footer.php';?>
